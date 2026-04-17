9bcf13ff910dd36720a74dbff7072d9310961d02
KimDmitriyR <kdmitrikim@yandex.ru>
2026-04-17 14:51:37 +0300

    feat(ml): integrate YOLO

--- ml/server.py
+++ ml/server.py
@@ -1,19 +1,41 @@
 from flask import Flask, request, jsonify
+from ultralytics import YOLO
+import base64
+import numpy as np
+import cv2
 
 app = Flask(__name__)
+
+model = YOLO("yolov8n.pt") 
 
 @app.route("/detect", methods=["POST"])
 def detect():
     data = request.json
-    frame = data.get("frame")
+    frame_data = data.get("frame")
 
-    print("Frame received")
 
-    return jsonify({
-        "boxes": [
-            {"x": 100, "y": 100, "w": 200, "h": 150}
-        ]
-    })
+    header, encoded = frame_data.split(",", 1)
+    img_bytes = base64.b64decode(encoded)
+
+    np_arr = np.frombuffer(img_bytes, np.uint8)
+    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+    results = model(img)[0]
+
+    boxes = []
+
+    for box in results.boxes:
+        cls = int(box.cls[0])
+        if cls == 0: 
+            x1, y1, x2, y2 = box.xyxy[0].tolist()
+            boxes.append({
+                "x": int(x1),
+                "y": int(y1),
+                "w": int(x2 - x1),
+                "h": int(y2 - y1)
+            })
+
+    return jsonify({"boxes": boxes})
 
 if __name__ == "__main__":
     app.run(port=5000)
